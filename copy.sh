44f0b2dfc090b31dd3fd9f3e8bbc3ce41ec9754a
Alexandre Bourget <alex@bourget.cc>
2018-04-25 11:36:45 -0400

    Clean-up of the repo.

--- copy.sh
+++ copy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 cp -v testnet-group2.yaml /keybase/public/abourget/
-cp -v testnet-group2-bootseq.yaml /keybase/public/abourget/
+cp -v bootseq-dawn3.yaml /keybase/public/abourget/
+cp -v testnet-docker.yaml /keybase/public/abourget/
+cp -v testnet-local.yaml /keybase/public/abourget/
